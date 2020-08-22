5bb74eb90f195d14283db622fb358b7966517770
Samuel Quintana <sock.sqt@gmail.com>
2020-08-21 22:32:34 -0500

    Updated tests for Maybe and Flowable

--- src/test/java/com/oz/learningjavarx/VogellaTutorial.java
+++ src/test/java/com/oz/learningjavarx/VogellaTutorial.java
@@ -1,7 +1,14 @@
 package com.oz.learningjavarx;
 
+import io.reactivex.Maybe;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableMaybeObserver;
+import io.reactivex.observers.DisposableObserver;
 import org.junit.Test;
 import io.reactivex.Observable;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,5 +30,69 @@ public class VogellaTutorial {
       System.out.printf("[%s] Just printing value: %s\n", Thread.currentThread().getId(), itemEmitted);
     });
     assertEquals("Hello", result);
+  }
+
+  /**
+   * Observable<T>: Emits 0 or n items and terminates with an success or an error event.
+   */
+  @Test
+  public void testWithObservable() {
+    Observable<String> stringObservable = Observable.create(emitter -> {
+      try {
+        List<String> todos = this.getTodos();
+        for (String todo : todos) {
+          emitter.onNext(todo);
+        }
+        emitter.onComplete();
+      } catch (Exception e) {
+        emitter.onError(e);
+      }
+    });
+  }
+
+  /**
+   * Maybe<T>: Succeeds with an item, or no item, or errors. The reactive version of an Optional.
+   */
+  @Test
+  public void testWithMaybe() {
+    Maybe<List<String>> todoMaybe = Maybe.create(emitter -> {
+      try {
+        List<String> todos = this.getTodos();
+        if (todos != null && !todos.isEmpty()) {
+          emitter.onSuccess(todos);
+        } else {
+          emitter.onComplete();
+        }
+      } catch (Exception genExc) {
+        emitter.onError(genExc);
+      }
+    });
+
+    // I can use this way for subscribe
+    Disposable disposableSubscriber = todoMaybe.subscribe(System.out::print, Throwable::printStackTrace);
+
+    // or this one...
+    todoMaybe.subscribeWith(new DisposableMaybeObserver<List<String>>() {
+      @Override
+      public void onSuccess(List<String> strings) {
+
+      }
+
+      @Override
+      public void onError(Throwable e) {
+
+      }
+
+      @Override
+      public void onComplete() {
+
+      }
+    });
+
+    disposableSubscriber.dispose(); // Stops listening
+  }
+
+  private List<String> getTodos() {
+    return Arrays.asList("Hello", "Fucking", "World");
   }
 }
